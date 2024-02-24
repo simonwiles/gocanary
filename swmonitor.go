77ae00c9d0b378da05e67a1eb011afd34ab132d9
Simon Wiles <simonjwiles@gmail.com>
2024-02-24 14:41:47 -0800

    Add `-version` flag

--- swmonitor.go
+++ swmonitor.go
@@ -6,7 +6,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
+
+const AppVersion = "0.1"
+
+type response struct {
+	Version string           `json:"version"`
+	Disks   map[string]*disk `json:"disks"`
+}
 
 func jsonResponse(response any, w http.ResponseWriter) {
 	rJson, err := json.MarshalIndent(response, "", "    ")
@@ -24,8 +32,14 @@ func jsonResponse(response any, w http.ResponseWriter) {
 func middleware() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fileSystems, err := GetDisksMap()
+
+		response := response{
+			Version: AppVersion,
+			Disks:   fileSystems,
+		}
+
 		if err == nil {
-			jsonResponse(fileSystems, w)
+			jsonResponse(response, w)
 			return
 		}
 		w.WriteHeader(http.StatusInternalServerError)
@@ -36,8 +50,13 @@ func middleware() http.Handler {
 func main() {
 	host := flag.String("host", "localhost", "Port to run the server on")
 	port := flag.Uint("port", 8080, "Port to run the server on")
+	version := flag.Bool("version", false, "prints current app version")
 
 	flag.Parse()
+	if *version {
+		fmt.Printf("swmonitor %s\n", AppVersion)
+		os.Exit(0)
+	}
 
 	fmt.Printf("Listening on %s:%d...\n", *host, *port)
 	http.Handle("/", middleware())
